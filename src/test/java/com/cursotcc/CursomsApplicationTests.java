a7879a3422c04f1159c29f3e9fb5f05c40b5d889
Lucas Teles <teleselectric@gmai.com>
2021-08-28 18:51:51 -0300

    Projeto criado

---
+++ src/test/java/com/cursotcc/CursomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursotcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
